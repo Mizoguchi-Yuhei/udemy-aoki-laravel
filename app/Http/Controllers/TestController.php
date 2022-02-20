e73e4608d278414874b0c038ce1328eb3aaa5b00
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2022-02-20 21:25:46 +0900

    laravel ui npm install npm run dev

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 // modelのデータを持ってくる
 use App\Models\Test;
 
+// DBファサードをインポート
+use Illuminate\Support\Facades\DB;
+
 class TestController extends Controller
 {
     //
@@ -14,8 +17,21 @@ class TestController extends Controller
     {
         $values = Test::all();
 
+        $collection = collect([1, 2, 3, 4, 5, 6, 7]);
+
+        $chunks = $collection->chunk(4);
+
+        $chunks->toArray();
+
         // die + var_dump  中身確認に便利
         // dd($values);
+        // dd($chunks);
+
+        $tests = DB::table('tests')
+        // ->select('id', 'text')
+        ->get();
+
+        dd($tests);
 
         return view('tests.test', compact('values'));
     }
